cd5adbacb5f106448557e53ba4463672f2034b2e
Vy Huynh <vhh002@ucsd.edu>
2020-01-30 10:49:47 -0800

    node.js lab

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
 
-var index = require('./routes/index');
-var hello = require('./routes/hello');
+var index = require("./routes/index");
+var hello = require("./routes/hello");
+var project = require("./routes/project");
 // Example route
 // var user = require('./routes/user');
 
@@ -17,7 +18,7 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views',  path.join(__dirname, 'views'));
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
 app.use(express.favicon());
@@ -30,6 +31,7 @@ app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
@@ -38,6 +40,7 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/hello/:userName', hello.view);
+app.get('/project/:name', project.viewProject);
 // Example route
 // app.get('/users', user.list);
 
